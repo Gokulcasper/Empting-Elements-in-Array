32bd18082380577a071521d4cfb80e70b7212ee1
Gokulcasper <ronitcasper215@gmail.com>
2022-06-06 10:20:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Empting Elements in Array
+
+const number = [1, 2, 3, 4, 5, 6];
+number = []; //solution 1
+
+// number.length = 0; //solution 2
+
+// number.splice(0, number.length); // solution 3
+
+// while (number.length) number.pop(); // solution 4
+
+console.log(number);
+
+//Mostly we used solution 1
